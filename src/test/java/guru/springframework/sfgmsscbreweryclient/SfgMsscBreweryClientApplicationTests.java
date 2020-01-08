fdedf4d17e6c1f08943d8df75cbf75f7ffcd8026
mwunderlich <martin@wunderlich.com>
2020-01-08 07:19:34 +0100

    Initial commit

---
+++ src/test/java/guru/springframework/sfgmsscbreweryclient/SfgMsscBreweryClientApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgmsscbreweryclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgMsscBreweryClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
